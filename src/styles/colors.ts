19dcb89719e0efefc85d90f80a332335010a951f
grazialencar <graziellealencar3@gmail.com>
2026-05-17 18:33:26 -0300

    feat: Variaveis das principais cores do projeto a serem utilizadas

---
+++ src/styles/colors.ts
@@ -0,0 +1,38 @@
+// src/styles/colors.ts
+export const COLORS = {
+  // LARANJA (Base #FE3C04)
+  primary: {
+    '050': '#FFE2D9', // Cor 0.5
+    '100': '#FFCABB', // Cor 1
+    '200': '#FFAB92', // Cor 1.5
+    '300': '#FE8B6A', // Cor 2
+    '400': '#FE6337', // Cor 3
+    '500': '#FE3C04', // Cor 4 (Principal)
+    '600': '#C42D01', // Cor 5
+    '700': '#912101', // Cor 6
+  },
+
+  // VERDE-AZUL (Base #114050)
+  secondary: {
+    '050': '#5BBBDC', // Cor 0.5
+    '100': '#42B1D7', // Cor 1
+    '200': '#2A9EC6', // Cor 1.5
+    '300': '#2383A4', // Cor 2
+    '400': '#1A627A', // Cor 3
+    '500': '#114050', // Cor 4 (Principal)
+    '600': '#09222A', // Cor 5
+    '700': '#040D11', // Cor 6
+  },
+
+  // ÂMBAR (Base #FF9101)
+  tertiary: {
+    '050': '#FFEED7', // Cor 0.5
+    '100': '#FFE1B9', // Cor 1
+    '200': '#FFCF90', // Cor 1.5
+    '300': '#FFBD67', // Cor 2
+    '400': '#FFA734', // Cor 3
+    '500': '#FF9101', // Cor 4 (Principal)
+    '600': '#C36E00', // Cor 5
+    '700': '#905200', // Cor 6
+  },
+};
